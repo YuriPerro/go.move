93fe081dd9d0596cf68b9089186dacbaf8b444ef
Yuri Perro <yuribaumgartner@ice.ufjf.br>
2021-02-25 19:18:28 -0300

    Criando btn Dark e add lib icons

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from 'react'
 import styles from '../styles/components/DarkModeButton.module.css'
 
-export function DarkModeButton() {
-    return (
-        <div className={styles.divContainer}>
-            <button className={styles.BtnContainer}>
+import { FaCloudMoon, FaSun } from 'react-icons/fa';
 
-                <div className={styles.divCircle} />
-            </button>
+export function DarkModeButton() {
+
+    const [active, setActive] = useState(false);
+
+    function toggle() {
+        setActive(!active);
+    }
+
+    useEffect(() => {
+        active ?
+            document.getElementById('divContr').className = styles.divContainerDark
+            :
+            document.getElementById('divContr').className = styles.divContainer
+    }, [active])
+
+    return (
+        <div id="divContr" className={styles.divContainer}>
+            <FaSun size={15} className={styles.faSun} style={{ position: 'absolute', top: 5 }} color='#ffe923'/>
+            <FaCloudMoon size={15} className={styles.faMoon} style={{ position: 'absolute', bottom: 5 }} color='#4722ac'/>
+            <input onClick={toggle} type="checkbox" className={styles.BtnContainer} />
         </div>
     )
 }
